cda411fd41801fed35b9180ecaa30c2a07156cd1
Burak Koc <brk.kc.06@icloud.com>
2023-08-15 17:43:30 +0300

    modalUpdate

---
+++ front/scripts/modal.js
@@ -0,0 +1,7 @@
+function openModal(){
+    document.getElementById('modal-1').classList.add('open');
+
+}
+function closeModal(){
+    document.querySelector('.jw-modal.open').classList.remove('open');
+}
