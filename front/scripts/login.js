45a3e2ece54ff30b4e6bb2513ca5b15f17e61328
Burak Koc <brk.kc.06@icloud.com>
2023-09-07 14:56:41 +0300

    spaceLineDeleteInLoginJs

--- front/scripts/login.js
+++ front/scripts/login.js
@@ -7,49 +7,13 @@
         a.type="password"
     }
 }
-// class User{
-//    constructor(email,password,firstname,lastname){
-//     this.email=email;
-//     this.password=password;
-//     this.firstname= firstname;
-//     this.lastname = lastname
-//    }
-// }
-
-// newUser1 = new User('asd', 'asd', 'asd', 'asd')
-
-// // console.log(newUser1)
-
-// class User{
-//     email;
-//     password;
-//     firstname;
-//     lastname;
-// }
-
-// newUser2 = new User()
-// newUser2.email =
-
-// var user1 = new User();
-// user1.email="brkkc06";
-// user1.password= 1234;
-// // console.log(user1);
-
-// function oneriAlerti(form){
-//     var inputValue=form.inputbox.value;
-//     alert ("you typed" + inputValue);
-// }
-
 function login(e) {
     e.preventDefault()
-
     loginform = document.getElementById('loginform')
     inputEmail = loginform.elements.email.value
     inputPassword = loginform.elements.password.value
-
     fetch("http://127.0.0.1:3000/login", {
         method: "POST",
-
         headers : {
             "Content-type":"application/json"
         },
@@ -58,7 +22,6 @@ function login(e) {
            password :inputPassword
         })
     }).then(response => {
-        
         response.json().then(async json=> {
             alert(json.message)
             sessionStorage.setItem("userId", json.id)
@@ -66,9 +29,7 @@ function login(e) {
             const html = await fetch("/public/feed.html").then((response) => response.text());
             document.getElementById("content").innerHTML = html;
             getPosts();
-
         })
-        
     })
 }
 function getPosts() {
@@ -78,7 +39,6 @@ function getPosts() {
         headers: {
             "Content-type": "application/json"
         },
-
     }).then(res => {
         res.json().then(async json => {
             const posts = json.posts;
@@ -88,7 +48,6 @@ function getPosts() {
                 const postDiv = document.createElement('div');
                 const postHtml = await fetch("/public/post.html").then((response) => response.text());
                 postDiv.innerHTML = postHtml;
-
                 if (post.createdBy) {
                     const result = await fetch(`http://127.0.0.1:3000/getUserById/${post.createdBy}`, {
                         method: "GET",
@@ -112,11 +71,8 @@ function getPosts() {
                         for(const btnBrk of postDiv.getElementsByClassName("btn-brk"))
                         btnBrk.style.display = "none";
                     }
-                   
                     Array.from(postDiv.getElementsByClassName("zgl"))[0].innerHTML = userName;     
                     Array.from(postDiv.getElementsByClassName("golf"))[0].innerHTML = postText;
-                    
-
                     if(post.likedBy){
                         const likedByResult = await fetch(`http://127.0.0.1:3000/getUserById/${post.likedBy}`,{
                             method:"GET",
@@ -131,7 +87,6 @@ function getPosts() {
                         Array.from(postDiv.getElementsByClassName("ivm-view-attr"))[0].src = userPhoto;  
                     }
                     else 
-                       
                         for(const likethisDiv of postDiv.getElementsByClassName("likethis"))
                         likethisDiv.style.display = "none";
                     if (postMedia) {
@@ -141,7 +96,6 @@ function getPosts() {
                         Array.from(postDiv.getElementsByClassName("img-anka "))[0].src =await getFile(userProfilePhoto);
                        
                     }
-                   
                     const accessKey = post.createdBy
                     Array.from(postDiv.getElementsByClassName("btn-brk"))[0].accessKey = accessKey;
                     Array.from(postDiv.getElementsByClassName("tfn "))[0].innerHTML = followedByUser;
@@ -149,18 +103,13 @@ function getPosts() {
                     postsDiv.appendChild(postDiv);
                 }
             }
-
-            // alert(json.text)
         }).catch(err => {
-            // // console.log(err);
         })
     })
-    
 }
 function getButtonBrk(e) {
     const accessKey = event.target.accessKey;
     const loginUserId = sessionStorage.getItem("userId");
-
     fetch("http://127.0.0.1:3000/addFollower",{
         method:"POST",
         headers:{
