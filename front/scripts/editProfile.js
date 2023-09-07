ad7e029321f2b0f4a316a9245d97243e67c5d806
Burak Koc <brk.kc.06@icloud.com>
2023-09-07 15:18:35 +0300

    spaceLineRemoveInEditProfileJS

--- front/scripts/editProfile.js
+++ front/scripts/editProfile.js
@@ -4,44 +4,30 @@ async function saveUserImg(imgtype){
     const formData = new FormData();
     formData.append("image", document.getElementById(imgtype).files[0]);
     fetch("http://127.0.0.1:3000/saveImg", {method: "post", body: formData}).then(async (res) => {
-        if(imgtype === "profilePhotoUpload") imgSrc = await res.text()
-        
+        if(imgtype === "profilePhotoUpload") imgSrc = await res.text()        
         else imgSrcBckground =await res.text()
     });
 }
 function logFile(event) {
-    // 
     var reader = new FileReader();
-
     reader.onload = function (event) {
-        
         document.getElementsByClassName("editPhoto")[0].src = event.target.result;
     };
-
     reader.readAsDataURL(document.getElementById("profilePhotoUpload").files[0]);
     saveUserImg("profilePhotoUpload");
-   
-
 }
 function HandleSubmit(event) {
-
     event.preventDefault();
     logFile(event);
-    // if(!file.value.length)return;
-    // const reader = new FileReader();
-    // reader.onload = logFile;
-    // reader.readAsDataURL(file.files[0]);
 }
 const imgBackground = document.getElementsByClassName("editBackground");
 function logFileBackground(event) {
-
     var readerBackground = new FileReader();
     readerBackground.onload = function (event) {
         document.getElementsByClassName("editBackground")[0].src = event.target.result;
     }
     readerBackground.readAsDataURL(document.getElementById("backgroundPhotoUpload").files[0]);
     saveUserImg("backgroundPhotoUpload");
-    
 }
 function BackgroundHandleSubmit(event) {
     event.preventDefault();
@@ -88,45 +74,34 @@ function getProfileInfo() {
             Array.from(document.getElementsByClassName("editPass"))[0].value = getPass;
             if(getSkills){
                 const ul = document.getElementsByClassName("ulChips");
-                // const li = ul[0].querySelectorAll("li").forEach(li => li.remove());
                 getSkills.forEach(skill => {
                     let liSkill = `<li>${skill} <i class="fa-solid fa-xmark fa-2xs"  onclick="remove(this,'${skill}')"></i></li>`;
                     ul[0].insertAdjacentHTML("afterbegin", liSkill);
                     skills.push(skill) 
                 })
             }
-
-
         })
     })
 }
-
-
 function startEditProfile() {
     const ul = document.getElementsByClassName("ulChips");
     const removeButton = document.getElementById("removeButton");
     const input = document.getElementById("inputChips");
     input.addEventListener("keyup", addSkill)
 }
-
-
 function createSkill() {
     const ul = document.getElementsByClassName("ulChips");
     ul[0].querySelectorAll("li").forEach(li => li.remove());
-
-
     skills.slice().reverse().forEach(skill => {
         let liSkill = `<li>${skill} <i class="fa-solid fa-xmark fa-2xs"  onclick="remove(this,'${skill}')"></i></li>`;
         ul[0].insertAdjacentHTML("afterbegin", liSkill);
     })
 }
-
 function remove(element, skill) {
     let index = skills.indexOf(skill);
     skills.splice(index, 1);
     element.parentElement.remove();
 }
-
 function addSkill(e) {
     if(e.key ==","){
         const skill = e.target.value.replace(/\s+/g,' ')
@@ -136,9 +111,6 @@ function addSkill(e) {
         const y=  x.join("");
         console.log(y);  
         const newSkill =skill.split(",");
-        
-
-        
         if(skill.length > 1 && !skills.includes(skill)){
             skill.split(' , ').forEach(skill =>{
                 skills.push(newSkill[0])
@@ -146,21 +118,16 @@ function addSkill(e) {
             });
         }
         e.target.value = "";
-    
     }
 }
-
 function removeButtonActivity(event) {
     const ul = document.getElementsByClassName("ulChips");
     skills.length = 0;
     ul[0].querySelectorAll("li").forEach(li => li.remove());
 }
-
-
 function onChangeInput() {
     document.getElementsByClassName('changeSaveBtn')[0].disabled = false;
 }
-
 function updateUser(e) {
     const loginUserId = sessionStorage.getItem("userId");
     e.preventDefault();
@@ -171,8 +138,6 @@ function updateUser(e) {
     const updateBackground = document.getElementsByClassName("editBackground")[0].src;
     const updateProfilePhoto = document.getElementsByClassName("editPhoto")[0].src;
     const updateSkill = skills;
-  
-
     fetch("http://127.0.0.1:3000/updateUser", {
         method: "Post",
         headers: {
@@ -193,5 +158,4 @@ function updateUser(e) {
     }).then(res => {
         res.json().then(async json => alert(json.message))
     })
-
 }
