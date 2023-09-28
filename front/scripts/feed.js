4d05684be6efcc4d0ba0f7abed2bcbc87979b2e2
Burak Koc <brk.kc.06@icloud.com>
2023-09-28 17:11:34 +0300

    ""

--- front/scripts/feed.js
+++ front/scripts/feed.js
@@ -1,35 +1,48 @@
-function getAllUser(){
-    const imgDiv = document.getElementsByClassName("person-photos");
-const nameDiv = document.getElementsByClassName("href-name-style");
-const depDiv = document.getElementsByClassName("a-text-style")
-console.log(nameDiv)
-getAllUserServices().then(res => {
+function getAllUser() {
+  const imgDiv = document.getElementsByClassName("person-photos");
+  const nameDiv = document.getElementsByClassName("href-name-style");
+  const depDiv = document.getElementsByClassName("a-text-style")
+  getAllUserServices().then(res => {
     res.json().then(async json => {
-        const users = json.users;
-        const usersArray = Array.from(users);
-        const shuffledArray = usersArray.sort(() => 0.5 - Math.random());
-        const n = 3 ;
-        const randomUsers = shuffledArray.slice(0,n);
+      const users = json.users;
+      const usersArray = Array.from(users);
+      const filtredArray = usersArray.filter((user) => user._id !=loginUserId);
+      const shuffledArray = filtredArray.sort(() => 0.5 - Math.random());
+      const n = 3;
+      const randomUsers = shuffledArray.slice(0, n);
+      randomUsers.forEach(async (user, index) => {
+        const userId = user._id;
+        const accessKey = userId;
+        Array.from(document.getElementsByClassName("rightFollowButton"))[index].accessKey = accessKey;
+        const userFirstName = user.firstName;
+        const userLastName = user.lastName;
+        const userFullName = userFirstName.charAt(0).toUpperCase() + userFirstName.slice(1) + '\xa0' + userLastName.charAt(0).toUpperCase() + userLastName.slice(1);
+        const userCompany = user.companyOrSchool;
+        const userDepartment = user.department;
+        const userInfo = userCompany + '<br>' +   userDepartment;
+        const userProfilePhoto = user.profilePhoto;
+        Array.from(nameDiv)[index].innerHTML = userFullName;
+        // default olarak db de fotoğraf olması lazım ekle !
+        if (!userProfilePhoto) {
+          Array.from(imgDiv)[index].src = 'assets/nonprofilephoto.png'
+        } else {
+          Array.from(imgDiv)[index].src = await getFile(userProfilePhoto);
+        }
+        if (userCompany) Array.from(depDiv)[index].innerHTML = userInfo;
+        else Array.from(depDiv)[index].innerHTML = ""
+      });
 
-        randomUsers.forEach(async (user, index) => {
-          const userId = user._id;
-          const userFirstName = user.firstName;
-          const userLastName = user.lastName;
-          const userFullName = userFirstName.charAt(0).toUpperCase()+ userFirstName.slice(1) + '\xa0' + userLastName.charAt(0).toUpperCase()+userLastName.slice(1);
-          const userDepartment = user.department;
-          const userProfilePhoto = user.profilePhoto;
-          Array.from(nameDiv)[index].innerHTML = userFullName;
-          // default olarak db de fotoğraf olması lazım ekle !
-          if (!userProfilePhoto) {
-            Array.from(imgDiv)[index].src = 'assets/nonprofilephoto.png'
-          } else {
-                Array.from(imgDiv)[index].src = await getFile(userProfilePhoto);       
-          }
-          if(userDepartment) Array.from(depDiv)[index].innerHTML = userDepartment;
-          else  Array.from(depDiv)[index].innerHTML = ""
-        });
-   
     })
-});
+  });
+}
 
+function follow(event){
+  const loginUserId = localStorage.getItem("userId");
+  const accessKey = event.target.accessKey;
+  console.log(accessKey)
+  addFollowerServices(loginUserId,accessKey)
+  .then((res) => res.text())
+  .then(async (res) => {
+      alert(res);
+  })
 }
